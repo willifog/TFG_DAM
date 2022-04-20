97a36ed4df96d2f18ec7b72fdd4bee12f332da68
Cesar Ramirez <cesarramirez1595@gmail.com>
2022-04-20 20:37:07 +0200

    solucion errores

---
+++ app/src/main/java/com/example/mistareas/providers/PostProvider.java
@@ -0,0 +1,20 @@
+package com.example.mistareas.providers;
+
+import com.example.mistareas.models.Post;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+public class PostProvider {
+
+    CollectionReference mCollection;
+
+    public PostProvider() {
+        mCollection = FirebaseFirestore.getInstance().collection("Posts"); //Creamos una nueva coleccion en FireStore
+    }
+
+    public Task<Void> save(Post post){  //Le pasamos la publicacion que vamos a guardar en "POSTS"
+        return mCollection.document().set(post);   //Lo ponemos vacio para que nos cree un nuevo documento unico para cada post.
+    }
+}
